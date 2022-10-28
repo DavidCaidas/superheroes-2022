8f34b21cf9097263e354f13bf133409016c253fd
Chema Pramos <jmperezra@gmail.com>
2022-10-28 15:33:33 +0200

    Proyecto creado.

---
+++ app/src/main/java/com/iesam/superheroe/SuperHeroesFeedActivity.kt
@@ -0,0 +1,11 @@
+package com.iesam.superheroe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SuperHeroesFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superheroe_feed)
+    }
+}
