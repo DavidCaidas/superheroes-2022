4db4a578306cc0ad1d4d979f92275b66a59c0530
Chema Pramos <jmperezra@gmail.com>
2022-10-28 15:59:50 +0200

    Creado dominio para el feed de superheroes

---
+++ app/src/main/java/com/iesam/superheroe/domain/Repositories.kt
@@ -0,0 +1,13 @@
+package com.iesam.superheroe.domain
+
+interface SuperHeroeRepository {
+    fun getSuperHeroe(): List<SuperHeroe>
+}
+
+interface BiographyRepository {
+    fun getBiography(superHeroeId: Int): Biography
+}
+
+interface WorkRepository {
+    fun getWork(superHeroeId: Int): Work
+}
